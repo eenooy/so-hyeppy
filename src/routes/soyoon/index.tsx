ec1efe6a93d82479f5de71ec2e462e9c084ee7e8
eenooyos <eenooyos@gmail.com>
2023-01-16 10:21:27 +0900

    Add: soyoon 브랜치 생성

--- src/routes/soyoon/index.tsx
+++ src/routes/soyoon/index.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useRecoilState } from 'recoil';
 
 const SoyoonPage: React.FC = () => {
-  return <div>SoyoonPage</div>;
+  return <div>SoyoonPage 입니다.</div>;
 };
 
 export default SoyoonPage;
