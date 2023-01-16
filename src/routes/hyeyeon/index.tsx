c301597002ef812a068a94313a65e57bd87aae6f
조혜연 <hy_312@mncnext.co.kr>
2023-01-16 10:38:42 +0900

    Add: hyeyeon page생성

--- src/routes/hyeyeon/index.tsx
+++ src/routes/hyeyeon/index.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useRecoilState } from 'recoil';
 
 const HyeyeonPage: React.FC = () => {
-  return <div>HyeyeonPage 입니다.</div>;
+  return <div>HyeyeonPage 입니다..</div>;
 };
 
 export default HyeyeonPage;
