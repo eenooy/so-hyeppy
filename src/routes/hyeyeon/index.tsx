52563d99456c16362c2701528c4760f882c30f42
eenooyos <eenooyos@gmail.com>
2023-01-16 10:16:35 +0900

    Add: 페이지 나누기

---
+++ src/routes/hyeyeon/index.tsx
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+import { useRecoilState } from 'recoil';
+
+const HyeyeonPage: React.FC = () => {
+  return <div>Login</div>;
+};
+
+export default HyeyeonPage;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,12 +2,17 @@ import { Routes, Route } from 'react-router-dom';
 import Layout from '../components/Layout';
 import Intro from './Intro';
 import Login from './Intro/Login';
+import HyeyeonPage from './hyeyeon';
+import SoyoonPage from './soyoon';
+
 const App = () => {
   return (
     <Layout>
       <Routes>
         <Route path="/" element={<Intro />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/hyeyeon" element={<HyeyeonPage />} />
+        <Route path="/soyoon" element={<SoyoonPage />} />
       </Routes>
     </Layout>
   );
