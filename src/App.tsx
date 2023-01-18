f7dcf37660ea1519f96dc49944f70a7c32aedbaf
박소윤 <eenooyos@gmail.com>
2023-01-18 23:57:52 +0900

    Add: recoil 사용해보기

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './Layout';
 import Intro from './Pages/Intro';
 import Login from './Pages/Intro/Login';
-import HyeyeonPage from './Pages/hyeyeon';
-import SoyoonPage from './Pages/soyoon';
+import HyeyeonPage from './Pages/Hyeyeon';
+import SoyoonPage from './Pages/Soyoon';
+import SoyoonLogin from './Pages/Soyoon/Login';
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
           <Route path="/login" element={<Login />} />
           <Route path="/hyeyeon" element={<HyeyeonPage />} />
           <Route path="/soyoon" element={<SoyoonPage />} />
+          <Route path="/soyoon/login" element={<SoyoonLogin />} />
         </Route>
       </Routes>
     </BrowserRouter>
