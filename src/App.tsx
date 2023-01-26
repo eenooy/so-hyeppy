09b29b7dd84a4791cef2da75d89cf140289ab17a
박소윤 <eenooyos@mncnext.co.kr>
2023-01-26 16:55:31 +0900

    modify: 페이지 루트 수정

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,7 @@ import Intro from './Pages/Intro';
 import Login from './Pages/Intro/Login';
 import HyeyeonPage from './Pages/Hyeyeon';
 import HyeyeonLogin from './Pages/Hyeyeon/Login';
-import SoyoonPage from './Pages/Soyoon';
-import SoyoonLogin from './Pages/Soyoon/Login';
+import Main from './Pages/Main';
 import './Styles/tailwind.css';
 
 const App = () => {
@@ -17,8 +16,7 @@ const App = () => {
           <Route path="/login" element={<Login />} />
           <Route path="/Hyeyeon" element={<HyeyeonPage />} />
           <Route path="/Hyeyeon/login" element={<HyeyeonLogin />} />
-          <Route path="/Soyoon" element={<SoyoonPage />} />
-          <Route path="/Soyoon/login" element={<SoyoonLogin />} />
+          <Route path="/main" element={<Main />} />
         </Route>
       </Routes>
     </BrowserRouter>
